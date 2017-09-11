9b3444d4a4060e07ab6fcfac10ba9eca4f0a2625
Спасибо <ty662@naver.com>
2017-09-11 13:22:32 +0900

    Create gcd_func.js

---
+++ gcd_func.js
@@ -0,0 +1,14 @@
+var r1,r2,r_tmp;
+var _arr=[];
+function gcd(a,b){
+	_arr[0]=a;_arr[1]=b;
+	_arr=_arr.sort();
+	console.log(_arr);
+	r1=_arr[0];r2=_arr[1];
+	r_tmp=0;
+	while(r2!=0){
+		r_tmp=r1%r2;
+		r1=r2;r2=r_tmp;
+    }
+	console.log("r is "+r1);		
+}
