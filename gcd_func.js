3698a522dffdce2fdcaf752ddde69dae6a57fe9e
Спасибо <ty662@naver.com>
2017-09-14 15:23:39 +0900

    Update gcd_func.js

--- gcd_func.js
+++ gcd_func.js
@@ -2,7 +2,7 @@ var r1,r2,r_tmp;
 var _arr=[];
 function gcd(a,b){
 	_arr[0]=a;_arr[1]=b;
-	_arr=_arr.sort();
+	_arr=_arr.sort(function(a,b){return a-b;});
 	console.log(_arr);
 	r1=_arr[0];r2=_arr[1];
 	r_tmp=0;
